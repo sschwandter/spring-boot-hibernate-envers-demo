b11548e29e729901e060900d24f4105d53a3e83c
Stefan Schwandter <s.schwandter@me.com>
2022-01-17 15:29:13 +0100

    Write tests.
    Just an aside: there _are_ naming conventions for queries, like findAllBy{FieldName}{Modifier}... want to show it to Stefan.

--- src/main/java/com/ns/springboothibernateenvers/UserDetailsRepository.java
+++ src/main/java/com/ns/springboothibernateenvers/UserDetailsRepository.java
@@ -3,6 +3,10 @@ package com.ns.springboothibernateenvers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Integer> {
+
+	List<UserDetails> findAllByLastNameContains(String name);
 }
--- src/test/java/com/ns/springboothibernateenvers/SpringBootHibernateEnversApplicationTests.java
+++ src/test/java/com/ns/springboothibernateenvers/SpringBootHibernateEnversApplicationTests.java
@@ -1,14 +1,88 @@
 package com.ns.springboothibernateenvers;
 
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import java.sql.Date;
+import java.time.Instant;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
 @SpringBootTest
+@AutoConfigureTestEntityManager
 class SpringBootHibernateEnversApplicationTests {
 
+    public static final int USER_ID = 1;
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    UserDetailsRepository userRepository;
 
     @Test
-    void contextLoads() {
+    @Transactional
+    void findByCurrentDateReturnsCurrentRevision() {
+
+        final AuditReader auditReader = AuditReaderFactory.get(entityManager.getEntityManager());
+
+        assertThat(entityManager, is(notNullValue()));
+        UserDetails userDetails = new UserDetails(USER_ID, "NIRAJ", "SONAWANE");
+
+        userRepository.save(userDetails);      // Create
+
+        userDetails.setFirstName("Updated Name");
+        userRepository.save(userDetails); // Update-1
+
+        userDetails.setLastName("Updated Last name"); // Update-2
+        userRepository.save(userDetails);
+
+        userRepository.delete(userDetails);
+
+        entityManager.getEntityManager().getTransaction().commit();
+
+        // Das nur nebenbei... spring data jpa CRUD repos HABEN default methoden
+        assertThat(userRepository.findAllByLastNameContains("Updated").size(), equalTo(1));
+
+        final UserDetails currentUserDetails = auditReader.find(UserDetails.class, USER_ID, Date.from(Instant.now()));
+
+
+        assertThat(currentUserDetails, is(nullValue()));
+
+    }
+
+    @Test
+    @Transactional
+    void ifCurrentEntryWasDeletedFindByCurrentDateReturnsNull() {
+
+        final AuditReader auditReader = AuditReaderFactory.get(entityManager.getEntityManager());
+
+        assertThat(entityManager, is(notNullValue()));
+        UserDetails userDetails = new UserDetails(USER_ID, "NIRAJ", "SONAWANE");
+
+        userRepository.save(userDetails);      // Create
+
+        userDetails.setFirstName("Updated Name");
+        userRepository.save(userDetails); // Update-1
+
+        userDetails.setLastName("Updated Last name"); // Update-2
+        userRepository.save(userDetails);
+
+        userRepository.delete(userDetails);
+
+        entityManager.getEntityManager().getTransaction().commit();
+
+
+        final UserDetails currentUserDetails = auditReader.find(UserDetails.class, USER_ID, Date.from(Instant.now()));
+
+
+        assertThat(currentUserDetails, is(nullValue()));
 
     }
 
